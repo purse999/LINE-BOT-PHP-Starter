4fd16921349561605e5b17002895d3589baf6cd7
purse999 <pipop09@gmail.com>
2017-01-12 16:32:11 +0700

    Update push.php

--- push.php
+++ push.php
@@ -10,7 +10,9 @@ $post = json_encode($data);
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);

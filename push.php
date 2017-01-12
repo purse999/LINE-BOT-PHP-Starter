29efd28eef66a25e9943a0f1285c4fa6d91f707b
purse999 <pipop09@gmail.com>
2017-01-12 16:26:57 +0700

    Update push.php

--- push.php
+++ push.php
@@ -2,7 +2,11 @@
 $access_token = 'qcK6VUxIi13MOHbAhXh5IF+1QAszI95wkVlMfpI9rnDRfKvQVbC5WwQp/a38qK4/aRabEiuL2MaF9gmpRo3QW+9yT9rdheE3UK1PTKw8tWZw4ULUGLIGsNy51vbzdG75jin7QYUyUJPBmmrdS1DRUQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/push';
-
+$data = [
+		'to' => 'U97dd5de82024c6e2bca31561df3c7aee',
+		'messages' => ['Hello'],
+	];
+$post = json_encode($data);
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);

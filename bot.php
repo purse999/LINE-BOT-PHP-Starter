08d424669989a3dd806afd2b1832d7879fee8f77
purse999 <pipop09@gmail.com>
2017-01-12 18:04:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'hello'
+				'text' => 'อย่าพิมพ์เล่น'
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
